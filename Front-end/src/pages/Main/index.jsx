a063ddaed8890b416dea588bfe68de10c8191e91
sungyoon <lsy8658>
2021-10-18 21:29:52 +0900

    feat : branch test (테스트)

--- Front-end/src/pages/Main/index.jsx
+++ Front-end/src/pages/Main/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const index = () => {
-  return <div>Main</div>;
+  return <div>Main123</div>;
 };
 
 export default index;
--- Front-end/src/pages/Register/index.jsx
+++ Front-end/src/pages/Register/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import {RegisterWrap,RegisterBox,FormTitle,FormSt,InputSt,ErrMs,FormBtn,LinkCon } from './styles';
 
-const index = () => {
-  return <div>Register</div>;
+const Register = () => {
+  return (
+    <RegisterWrap>
+      <RegisterBox>
+        <FormTitle>회원가입</FormTitle>
+        <FormSt>
+          <InputSt type="email" placeholder="이메일"></InputSt>
+          <InputSt type="text" placeholder="성함"></InputSt>
+          <InputSt type="password" placeholder="비밀번호"></InputSt>
+          <InputSt type="password" placeholder="비밀번호 확인"></InputSt>
+          <ErrMs>비밀번호 형식이 맞지 않습니다.</ErrMs>
+          <FormBtn>가입하기</FormBtn>
+          <LinkCon>
+            <Link to="/">돌아가기</Link>
+          </LinkCon>
+        </FormSt>
+      </RegisterBox>
+    </RegisterWrap>
+  );
 };
-
-export default index;
+export default Register;
