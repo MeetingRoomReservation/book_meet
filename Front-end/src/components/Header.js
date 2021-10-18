99e399cf5b98bb6c1b00a77e3dbeae52cdc04798
Namukk <skadnr625@gmail.com>
2021-10-18 20:27:34 +0900

    feat : header 작성 컴포넌트 header 구현

--- Front-end/src/components/Header.js
+++ Front-end/src/components/Header.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  max-width: 100%;
+  padding: 20px;
+  color: white;
+  background-color: #344551;
+`;
+
+const Title = styled.div`
+  font-size: 35px;
+`;
+
+const BtnContainer = styled.div`
+  margin: 10px;
+`;
+
+const BtnList = styled.span`
+  padding: 10px;
+`;
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <MainContainer>
+      <Title>
+        <Link to="/main">회의 관리 시스템</Link>
+      </Title>
+      <BtnContainer>
+        <BtnList>
+          <Link to="/users">마이페이지</Link>
+        </BtnList>
+        <BtnList>
+          <Link to="/rooms">회의실</Link>
+        </BtnList>
+        <BtnList>
+          <Link to="/minutes">회의록</Link>
+        </BtnList>
+        <BtnList>
+          <Link to="/">로그아웃</Link>
+        </BtnList>
+      </BtnContainer>
+    </MainContainer>
+  );
 };
 
 export default Header;
