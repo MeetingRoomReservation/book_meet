9de80c1963f3e20b01ac902b81675805f1eb74cc
scseong <goidtv@naver.com>
2021-10-18 15:43:48 +0900

    first commit

---
+++ Front-end/src/components/Router.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const Login = loadable(() => import('@pages/Login'));
+const Main = loadable(() => import('@pages/Main'));
+const Register = loadable(() => import('@pages/Register'));
+const Minutes = loadable(() => import('@pages/Minutes'));
+const Users = loadable(() => import('@pages/Users'));
+const Notices = loadable(() => import('@pages/Notices'));
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/main" component={Main} />
+        <Route path="/register" component={Register} />
+        <Route path="/users" component={Users} />
+        <Route path="/minutes" component={Minutes} />
+        <Route path="/notices" component={Notices} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ Front-end/src/pages/Notices/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>Notices</div>;
+};
+
+export default index;
