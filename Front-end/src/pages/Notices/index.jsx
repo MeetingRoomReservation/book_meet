4c6fb0953afbf326d2b278a60b8c0a0bf60a0b20
scseong <goidtv@naver.com>
2021-10-23 22:13:00 +0900

    feat: 공지사항 리스트 및 작성하기 공지사항 테이블 수정 및 작성 페이지 구현

--- Front-end/src/pages/Notices/index.jsx
+++ Front-end/src/pages/Notices/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import Header from '@components/Header';
 import NoticeList from '@components/post/NoticeList';
+import PostWriting from '@components/post/PostWriting';
+import PostView from '@components/post/PostView';
 
-const index = () => {
-  return <NoticeList />;
+import { Switch, Route } from 'react-router-dom';
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/notices/writing" component={PostWriting} />
+        <Route path="/notices/:id" component={PostView} />
+        <Route path="/notices" component={NoticeList} />
+      </Switch>
+    </>
+  );
 };
 
-export default index;
+export default Index;
