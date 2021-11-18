e479b15cc9f323c1e8197bcd9ca16e4409bdb44b
scseong <goidtv@naver.com>
2021-11-18 17:56:13 +0900

    feat: 공지사항 글쓰기 에디터 글쓰기 폼 구현

---
+++ Front-end/src/pages/Notices/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const NoticeContainer = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
